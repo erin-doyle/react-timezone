6ffff2b487a1239c7193e3105ee352405d26dc05
Erin Doyle <edoyle@cashstar.com>
2017-08-30 10:09:31 -0400

    Added react-autocomplete and replaced the react-bootstrap-typeahead with it.

--- src/components/TimezonePicker.js
+++ src/components/TimezonePicker.js
@@ -1,43 +1,76 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Typeahead } from 'react-bootstrap-typeahead';
+import classNames from 'classnames/dedupe';
+import Autocomplete from 'react-autocomplete';
 
 import timeHelper from '../utils/time';
 
-import BackButton from './BackButton';
+
+export const TimezoneOption = (item, isHighlighted) => (
+    <div
+        key={`${item.city}-${item.zoneAbbr}`}
+        className={classNames('timezone-option', {
+            'timezone-option__highlighted': isHighlighted
+        })}
+    >
+        {timeHelper.tzDisplay(item)}
+    </div>
+);
 
 
 class TimezonePicker extends React.PureComponent {
     constructor(props) {
         super(props);
 
+        this.state = {
+            timezone: timeHelper.guessUserTz()
+        };
+
         this.handleTimezoneChange = this.handleTimezoneChange.bind(this);
     }
 
     handleTimezoneChange(selection) {
-        const { onTimezoneChange, onClearFocus } = this.props;
+        const { onTimezoneChange } = this.props;
         const zoneObject = selection[0];
         if (zoneObject) {
+            this.setState({ timezone: zoneObject });
             if (onTimezoneChange) onTimezoneChange(zoneObject);
-            onClearFocus();
         }
     }
 
     render() {
-        const { phrases } = this.props;
+        const {
+            phrases,
+            inputProps,
+            menuStyle,
+            onMenuVisibilityChange,
+            wrapperProps
+        } = this.props;
+
+        const { timezone } = this.state;
+
+        const valueDisplay = `${timezone.zoneName} ${timezone.zoneAbbr}`;
+
         return (
             <div className="timezone_picker_container">
                 <div className="timezone_picker_search">
                     <label id="timezone-picker-search-label" htmlFor="timezone-picker-search-input">
                         {phrases.timezonePickerLabel}
                     </label>
-                    <Typeahead
+                    <Autocomplete
                         id="timezone-picker-search-input"
-                        onChange={this.handleTimezoneChange}
-                        labelKey={option => `${option.city} - ${option.zoneAbbr}`}
-                        options={timeHelper.tzMaps}
-                        maxResults={5}
-                        minLength={3}
+                        onChange={(event, value) => this.handleTimezoneChange(value)}
+                        onSelect={value => this.handleTimezoneChange(value)}
+                        onMenuVisibilityChange={onMenuVisibilityChange}
+                        items={timeHelper.tzMaps}
+                        getItemValue={timeHelper.tzDisplay}
+                        value={valueDisplay}
+                        menuStyle={menuStyle}
+                        inputProps={inputProps}
+                        wrapperProps={wrapperProps}
+                        shouldItemRender={timeHelper.isValueInCityOrZone}
+                        sortItems={timeHelper.compareByCityAndZone}
+                        renderItem={TimezoneOption}
                     />
                 </div>
             </div>
@@ -49,14 +82,29 @@ TimezonePicker.defaultProps = {
     phrases: {
         timezonePickerLabel: 'Closest City or Timezone'
     },
-    onClearFocus: () => {},
-    onTimezoneChange: () => {}
+    onTimezoneChange: () => {},
+    onMenuVisibilityChange: () => {},
+    inputProps: {},
+    menuStyle: {
+        borderRadius: '2px',
+        boxShadow: '0 2px 12px rgba(0, 0, 0, 0.1)',
+        background: 'rgba(255, 255, 255, 0.9)',
+        fontSize: '1rem',
+        position: 'sticky',
+        overflow: 'auto',
+        width: '100%',
+        minWidth: 'initial'
+    },
+    wrapperProps: {}
 };
 
 TimezonePicker.propTypes = {
     phrases: PropTypes.object,
-    onClearFocus: PropTypes.func, // TODO: can we remove this?
-    onTimezoneChange: PropTypes.func
+    onTimezoneChange: PropTypes.func,
+    onMenuVisibilityChange: PropTypes.func,
+    inputProps: PropTypes.object,
+    menuStyle: PropTypes.object,
+    wrapperProps: PropTypes.object
 };
 
 export default TimezonePicker;
--- test/components/TimezonePicker.test.js
+++ test/components/TimezonePicker.test.js
@@ -5,13 +5,12 @@ import TimezonePicker from '../../src/components/TimezonePicker';
 
 
 const mockPhrases = {
-    close: 'phrase for close',
-    timezonePickerTitle: 'phrase for the Title',
     timezonePickerLabel: 'phrase for the label'
 };
 const mockTimezone = {
+    city: 'Some City',
     zoneName: 'Some Zone',
-    zoneAbbr: 'SZ'
+    zoneAbbr: 'SOZ'
 };
 
 describe('TimezonePicker', () => {
@@ -22,57 +21,12 @@ describe('TimezonePicker', () => {
             />
         );
 
-        it('should render a header with a title', () => {
-            expect(wrapper.find('.timezone_picker_header_title').text()).toBe(mockPhrases.timezonePickerTitle);
-        });
-
         it('should render a label for the search field', () => {
             expect(wrapper.find('.timezone_picker_search').find('label').text()).toBe(mockPhrases.timezonePickerLabel);
         });
 
-        it('should render a Typeahead', () => {
-            expect(wrapper.find('OnClickOutside(Typeahead)').length).toBe(1);
-        });
-
-        it('should render a close button', () => {
-            expect(wrapper.find('button').text()).toBe(mockPhrases.close);
-        });
-    });
-
-    describe('onClearFocus func', () => {
-        it('should callback when onClick header "back" icon', () => {
-            const onFocusChangeStub = jest.fn();
-            const wrapper = shallow(
-                <TimezonePicker
-                    phrases={mockPhrases}
-                    onClearFocus={onFocusChangeStub}
-                />
-            );
-            expect(wrapper.find('BackButton').prop('clickHandler')).toBe(onFocusChangeStub);
-        });
-
-        it('should callback when onClick Button', () => {
-            const onFocusChangeStub = jest.fn();
-            const wrapper = shallow(
-                <TimezonePicker
-                    phrases={mockPhrases}
-                    onClearFocus={onFocusChangeStub}
-                />
-            );
-            wrapper.find('button').simulate('click');
-            expect(onFocusChangeStub.mock.calls.length).toBe(1);
-        });
-
-        it('should callback when timezone change', () => {
-            const onFocusChangeStub = jest.fn();
-            const wrapper = shallow(
-                <TimezonePicker
-                    phrases={mockPhrases}
-                    onClearFocus={onFocusChangeStub}
-                />
-            );
-            wrapper.instance().handleTimezoneChange([mockTimezone]);
-            expect(onFocusChangeStub.mock.calls.length).toBe(1);
+        it('should render an Autocomplete', () => {
+            expect(wrapper.find('Autocomplete').exists()).toBeTruthy();
         });
     });
 
@@ -82,7 +36,7 @@ describe('TimezonePicker', () => {
             const wrapper = shallow(
                 <TimezonePicker
                     phrases={mockPhrases}
-                    handleTimezoneChange={onTimezoneChangeStub}
+                    onTimezoneChange={onTimezoneChangeStub}
                 />
             );
             wrapper.instance().handleTimezoneChange([mockTimezone]);
