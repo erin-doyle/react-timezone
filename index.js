ca818579398f1999762894c28b95517d0941b101
Erin Doyle <edoyle@cashstar.com>
2017-09-06 13:26:21 -0400

    Missed updating the imported paths to the lib directory instead of src

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var TimezoneAutocomplete = require('./src/components/TimezoneAutocomplete').default;
-var timezoneShape = require('./src/utils/types').timezoneShape;
-var injectTimezone = require('./src/inject').default;
+var TimezoneAutocomplete = require('./lib/components/TimezoneAutocomplete').default;
+var timezoneShape = require('./lib/utils/types').timezoneShape;
+var injectTimezone = require('./lib/inject').default;
 
 module.exports = {
     TimezoneAutocomplete: TimezoneAutocomplete,
