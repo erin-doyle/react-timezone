e0b2e40568691230d7b5dfaac199e863659bc211
Erin Doyle <edoyle@cashstar.com>
2018-01-05 13:57:10 -0500

    Removed injectTimezone.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var TimezoneAutocomplete = require('./lib/components/TimezoneAutocomplete').default;
 var timezoneShape = require('./lib/utils/types').timezoneShape;
-var injectTimezone = require('./lib/inject').default;
 
 module.exports = {
     TimezoneAutocomplete: TimezoneAutocomplete,
-    timezoneShape: timezoneShape,
-    injectTimezone: injectTimezone
+    timezoneShape: timezoneShape
 };
