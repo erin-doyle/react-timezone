fcbfc7240968aef3db68c67f05036571d761cb5f
Erin Doyle <edoyle@cashstar.com>
2019-06-26 10:17:04 -0400

    Updated Storybook to the latest 5.1.9

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,15 @@
-module.exports = (baseConfig, env, defaultConfig) => ({
-    ...defaultConfig,
-    module: {
-        ...defaultConfig.module,
-        rules: [
-            // Temp fix for issue: https://github.com/storybooks/storybook/issues/3346
-            ...defaultConfig.module.rules.filter(rule => !(
-                (rule.use && rule.use.length && rule.use.find(({ loader }) => loader === 'babel-loader'))
-            )),
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            }
-        ],
-    },
-});
+module.exports = async ({ config }) => {
+    config.module.rules = [
+        // Temp fix for issue: https://github.com/storybooks/storybook/issues/3346
+        ...config.module.rules.filter(rule => !(
+            (rule.use && rule.use.length && rule.use.find(({ loader }) => loader === 'babel-loader'))
+        )),
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }
+    ];
+
+    return config;
+};
